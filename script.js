002c570638e5185f0055099a3e84d71c0273d6a2
naveen-kk4 <133044450+naveen-kk4@users.noreply.github.com>
2023-09-06 01:20:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+async function callme(event){
+    event.preventDefault();
+	let prom = await new Promise((resolve)=>{
+		setTimeout(()=>{
+            resolve(document.getElementById("text").value)
+        },document.getElementById("delay").value)
+		
+	});
+	document.getElementById("output").innerText = prom;
+	
+	
+}
+
